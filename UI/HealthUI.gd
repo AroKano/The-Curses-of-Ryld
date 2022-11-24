f5677c5ec23dec3fb916e897e9a2c7ff66d36967
AroKano <arkan.k@hotmail.de>
2022-11-24 02:06:19 +0100

    Test Commit

---
+++ UI/HealthUI.gd
@@ -0,0 +1,24 @@
+extends Control
+
+var hearts = 4 setget set_hearts
+var max_hearts = 4 setget set_max_hearts
+
+onready var heartUIFull = $HeartUIFull
+onready var heartUIEmpty = $HeartUIEmpty
+
+func set_hearts(value):
+	hearts = clamp(value, 0, max_hearts)
+	if heartUIFull != null:
+		heartUIFull.rect_size.x = hearts * 12 #  hearts * 12 weil pixelbreite von hearts 12 ist
+
+func set_max_hearts(value):
+	max_hearts = max(value, 1)
+	self.hearts = min(hearts, max_hearts)
+	if heartUIEmpty != null:
+		heartUIEmpty.rect_size.x = max_hearts * 12 #  max_hearts * 12 weil pixelbreite von max_hearts 12 ist
+
+func _ready():
+	self.max_hearts = PlayerStats.max_health
+	self.hearts = PlayerStats.health
+	PlayerStats.connect("health_changed", self, "set_hearts")
+	PlayerStats.connect("max_health_changed", self, "set_max_hearts")
